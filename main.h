fe1e644f641e6cd5b7786db5c32c8b065065aa53
khalil <maaizikhalil@gmail.com>
2023-08-24 03:32:56 -0700

    update

--- main.h
+++ main.h
@@ -1,65 +1,80 @@
-#ifndef main_h
-#define main_h
-
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <signal.h>
-#include <stdarg.h>
-#include <limits.h>
-#ifndef BUFF_SIZE
-#define BUFF_SIZE 300
-#endif
+#ifndef MAIN_H
+#define MAIN_H
 
 extern char **environ;
 
-/**
- * struct link - node that contains the directories
- * @dir: directory
- * @next: position to change the node
- */
-typedef struct link
-{char *dir;
-struct link *next;
-} link_t;
-/**
-* struct formats - struct formats
-* @ch: pointer to firts element.
-* @func: The function associated.
-*/
-typedef struct formats
-	{
-		char *ch;
-		int (*func)();
-	} form;
+/* header files */
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <dirent.h>
+#include <errno.h>
+#include <signal.h>
 
-char *_get_env(const char *nm);
-ssize_t _get_lin(char **bufferline, size_t *s, FILE *st);
-char **_splitline_(char *c_lin);
-int _exe_proc(char **arm, char **ar, int count);
-char *_which(link_t **head, char *av);
-char *_strcpy(char *dest, char *src);
-char *_strtok(char *s, char *d);
-char *_strcat(char *dir, char *sl, char *ar);
-int _strlen(char *str);
-link_t *_link(char *p);
-void free_list(link_t *head);
-link_t *_add_nodeint_end(link_t **head, char *d);
-int _putchar(char c);
-int p_char(va_list args);
-int p_int(va_list args);
-int p_str(va_list args);
-int _printf(const char *frmt, ...);
-char *check_path(char *arm);
-char *execute_command(char *arm);
-int main(int __attribute__((unused))ac, char **ar, char **env);
-int my_exit(char *l);
-int _env(char *l, int count, char **ar, char **env);
-void _signal(int sg);
-int _getchar(void);
+/**
+ * struct shell_env - stores address for pointer to free.
+ * @path_values: pointer to path values
+ * @input: pointer to buffer that getline creates.
+ * @input_token: pointers for input token.
+ * @modify_path: pointer to the path after it has been modified
+ *
+ * Description: provides a storage for all elements that are malloced.
+ */
+typedef struct shell_env
+{
+	char **path_values;
+	char *input;
+	char **input_token;
+	char *modify_path;
+} shell_t;
+
+/**
+ * struct built_in_cmd - struct for different built in commands.
+ * @cmd_name: name of the cmd
+ * @cmd: function pointer to run the cmd
+ *
+ * Description: struct for different built in commands in our shell
+ */
+typedef struct built_in_cmd
+{
+	char *cmd_name;
+	void (*cmd)(shell_t *);
+} built_t;
+
+/* main.c */
+int run_build_in(shell_t *, char *);
+int run_command(shell_t *, char *, char **);
+int run_path(shell_t *, char *);
+int check_slash(char *);
+
+/* string.c */
+size_t _strlen(char *);
+char *_strdup(char *);
+char **tokenize_str(char *, char *);
+int _strcmp(char *, char *);
+
+/* prompt_util.c */
+void print_ps1(int);
+char *find_pathname(char **, char *);
+char *_getenv(const char *);
+char *make_pathname(char *, char *);
+char **get_path(char **);
+
+/* prompt_util2.c */
+void free_shell_t(shell_t *);
+void p_commanderr(char *, char *);
+
+/* buildin.c */
+void my_exit(shell_t *);
+void print_env(shell_t *);
+
+/* function prototypes */
+char *_strtok(char *, const char *);
+ssize_t getline(char **, size_t *, FILE *);
+
 #endif
